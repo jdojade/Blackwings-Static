c46c78210c57425bcb04b7b21287c48ff83acec8
Jade Oloroso <100562473+jdojade@users.noreply.github.com>
2022-07-27 19:15:31 +0800

    Updates

---
+++ Slideshow/ViewModel/CatMViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ES_BLACKWINGS.ViewModel
+{
+    public class CatMViewModel
+    {
+        public System.Guid ItemId { get; set; }
+        public int CategoryId { get; set; }
+        public string ItemCode { get; set; }
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public HttpPostedFileBase ImagePath { get; set; }
+        public decimal ItemPrice { get; set; }
+
+        public IEnumerable<SelectListItem> CategorySelectListItem { get; set; }
+    }
+}
---
+++ Slideshow/ViewModel/ShoppingViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ES_BLACKWINGS.ViewModel
+{
+    public class ShoppingViewModel
+    {
+        public Guid ItemId { get; set; }
+        public String ItemName { get; set; }
+        public decimal ItemPrice { get; set; }
+        public string ImagePath { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string ItemCode { get; set; }
+        public int IsActive { get; set; }
+
+        public string Size { get; set; }
+
+    }
+}
